07d0acfd2cf8496a090cf6012cf042e03b593535
juniuszhou <junius.zhou@gmail.com>
2019-07-25 19:27:49 +0800

    check in code to get author, state and chain related api.

--- substrate/tests/src/main.rs
+++ substrate/tests/src/main.rs
@@ -9,31 +9,21 @@ extern crate node_runtime;
 
 use futures::Future;
 use hyper::rt;
-use jsonrpc_core_client::{
-    transports::http,
-    transports::ws,
-    RpcError,
-    TypedClient,
-    RpcChannel,
-};
+use jsonrpc_core_client::{transports::http, transports::ws, RpcError};
 use substrate_rpc::{system::SystemClient,
-                    chain::ChainClient,
+                    chain::{ChainClient, number::NumberOrHex},
                     state::StateClient,
-                    author::{
-                        AuthorClient,
-                        hash::ExtrinsicOrHash,
-                    },
-};
+                    author::{AuthorClient, hash::ExtrinsicOrHash, }, };
 use node_primitives::{Hash};
 use node_runtime::{Address, Block, Header, SignedBlock};
-fn main() {
 
+pub fn main() {
     rt::run(rt::lazy(|| {
         let uri = "http://192.168.2.158:9933";
 
         http::connect(uri)
-            .and_then(|client: AuthorClient<Hash, Hash>| {
-                get_author_api(&client)
+            .and_then(|client: ChainClient<Hash, Hash, Header, SignedBlock>| {
+                get_chain_api(&client)
             })
             .map_err(|e| {
                 println!("Error: {:?}", e);
@@ -41,11 +31,10 @@ fn main() {
     }));
 }
 
-
-fn get_author_api(client: &AuthorClient<Hash, Hash>) -> impl Future<Item=(), Error=RpcError> {
-    client.submit_extrinsic(vec![1, 2, 3].into())
-        .map(|result| {
-            println!("{:?}", result);
+fn get_chain_api(client: &ChainClient<Hash, Hash, Header, SignedBlock>) -> impl Future<Item=(), Error=RpcError> {
+    client.block_hash(Some(NumberOrHex::Number(1.into())))
+        .map(|block_hash| {
+            println!("{:?}", block_hash);
         })
 }
 
